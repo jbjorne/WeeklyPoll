41fcb293f751ae76f5dc3ffc259f54778ce432bf
jari <jari.bjorne@utu.fi>
2016-09-29 19:49:49 +0300

    Email generation appears to work.

--- sendMail.py
+++ sendMail.py
@@ -1,9 +1,10 @@
 import json
 import datetime
 import subprocess
+import codecs
 
 def readEmails(emailPath):
-    with open(emailPath) as f:    
+    with codecs.open(emailPath, "rt", "utf-8") as f:    
         emails = f.read()
     emails.replace("\n", " ").strip()
     emails = emails.split(",")
@@ -14,34 +15,51 @@ def readEmails(emailPath):
         address = address.replace(">", "").strip()
         print name, address
         data.append({"name":name, "email":address})
+    return data
 
 def runMailCommand(doodle, recipients, template):
-    command = "mail -s " + doodle["fullTitle"]
+    command = u"mail -s '" + doodle["title"] + "'"
     for recipient in recipients:
         if recipient["name"] != None:
-            command += "'" + recipient["name"] + " <" + recipient["email"] + ">"
+            command += " '" + recipient["name"] + " <" + recipient["email"] + ">'"
+        else:
+            command += " " + recipient["email"]
+    print "--------------------------"
     print command
+    print "--------------------------"
     template = template.replace("%doodle", doodle["link"])
     print template
+    print "--------------------------"
     #p = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
     #return p.communicate(input=content)
 
-def sendMail(doodlesPath, emailPath, daysInAdvance, teplatePath):
+def sendMail(doodlesPath, emailPath, daysInAdvance, templatePath):
     emails = readEmails(options.emails)
     
     with open(doodlesPath) as f:    
         doodles = json.load(f)
     
-    today = datetime.datetime.now()
+    with codecs.open(templatePath, "rt", "utf-8") as f:    
+        template = f.read()
+    
+    today = datetime.datetime.today()
+    print "Current time is", today
     for doodle in doodles:
+        print "Checking", doodle,
         sent = doodle.get("sent")
+        if sent == True:
+            print "already sent"
+            continue
         if sent == None:
             doodle["sent"] = False
         date = datetime.datetime.strptime(doodle["date"], "%Y%m%d")
-        delta = datetime.timedelta(today, date)
+        delta = date - today
         if delta.days <= daysInAdvance:
-            print "Sending email for doodle", doodle
+            print "Sending email now"
+            runMailCommand(doodle, emails, template)
             #doodle["sent"] = True
+        else:
+            print "checked, delta is", delta
     
     with open(doodlesPath, 'w') as fp:
         json.dump(doodles, fp)
@@ -51,8 +69,10 @@ if __name__=="__main__":
     optparser = OptionParser(description="Batch process a tree of input files")
     optparser.add_option("-d", "--doodles", default=None)
     optparser.add_option("-m", "--emails", default=None)
+    optparser.add_option("-t", "--template", default=None)
     optparser.add_option("-s", "--days", type=int, default=None)
     (options, args) = optparser.parse_args()
     
-    sendMail = readEmails(options.emails)
+    #sendMail = readEmails(options.emails)
+    sendMail(options.doodles, options.emails, options.days, options.template)
     
